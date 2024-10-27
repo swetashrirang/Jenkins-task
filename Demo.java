efcf59be314366ac0c8f4ec52fd9223b2d0dada3
swetashrirang <157368571+swetashrirang@users.noreply.github.com>
2024-10-27 11:28:05 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
 public class Demo{
-	public static void main (string [] args){
+	public static void main (String [] args){
 	System.out.println ("Hello from Demo");
 	    System.out.println ("Hello from demo java");
 	}
