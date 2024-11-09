1075122a72bb419ff0e1f294329cc2e0fdc2d5b3
swetashrirang <157368571+swetashrirang@users.noreply.github.com>
2024-11-09 12:16:44 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,5 +2,6 @@ public class Demo{
 	public static void main (String [] args){
 	System.out.println ("Hello from Demo");
 	    System.out.println ("Hello from demo java");
+		System.out.println ("Hello from Sweta");
 	}
 }
